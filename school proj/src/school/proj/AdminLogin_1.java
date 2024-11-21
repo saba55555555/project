d9ad8bfd7fda2894ee805f73a1749d63c0d72a22
saba55555555 <nsaba3324@gmail.com>
2024-11-21 18:14:06 +0500

    Add files via upload

---
+++ school proj/src/school/proj/AdminLogin_1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package school.proj;
+
+/**
+ *
+ * @author UC 313
+ */
+class AdminLogin {
+    
+}
